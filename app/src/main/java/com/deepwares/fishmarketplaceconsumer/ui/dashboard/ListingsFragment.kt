6d3dc62037abcf064b077ccef5a9d7979e0a8703
Deepak Sundar <deepak@airtime.com>
2020-07-22 04:23:40 +0530

    init repo

---
+++ app/src/main/java/com/deepwares/fishmarketplaceconsumer/ui/dashboard/ListingsFragment.kt
@@ -0,0 +1,31 @@
+package com.deepwares.fishmarketplaceconsumer.ui.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.deepwares.fishmarketplaceconsumer.R
+
+class ListingsFragment : Fragment() {
+
+    private lateinit var listingsViewModel: ListingsViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        listingsViewModel =
+                ViewModelProvider(this).get(ListingsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_listings, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        listingsViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
